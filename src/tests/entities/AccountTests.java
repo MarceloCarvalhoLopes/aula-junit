2526f624783c2721ed27cfee98e16efdd5d9b1af
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2024-08-08 11:18:21 -0300

    assertThrows

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -2,6 +2,7 @@ package tests.entities;
 import entities.Account;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import tests.factory.AccountFactory;
 
 public class AccountTests {
 
@@ -10,7 +11,7 @@ public class AccountTests {
         //Arrange -> prepared the data
         double amount = 200.0;
         double expectedValue = 196.0;
-        Account acc = new Account(1L,0.0);
+        Account acc = AccountFactory.createEmptyAccount();
 
         //Act -> Necessary actions
         acc.deposit(amount);
@@ -24,7 +25,7 @@ public class AccountTests {
         //Arrange -> prepared the data
         double amount = -200.0;
         double expectedValue = 200.0;
-        Account acc = new Account(1L,expectedValue);
+        Account acc = AccountFactory.createAccount(expectedValue);
 
         //Act -> Necessary actions
         acc.deposit(amount);
@@ -32,4 +33,53 @@ public class AccountTests {
         //Assert -> Expected value
         Assertions.assertEquals(expectedValue, acc.getBalance());
     }
+
+    @Test
+    public void fullWithdrawClearBalanceAndReturnFullBalance(){
+
+        //Arrange -> prepared the data
+        double expectedValue = 0.0;
+        double initialBalance = 800.0;
+        Account acc = AccountFactory.createAccount(initialBalance);
+
+        //Act -> Necessary actions
+        double result = acc.fullWithdraw();
+
+        //Assert -> Expected value
+        Assertions.assertEquals(expectedValue,acc.getBalance());
+        Assertions.assertEquals(result,initialBalance);
+
+    }
+
+    @Test
+    public void withdrawShouldDecreaseBalanceWhenSufficientBalance(){
+        //Arrange -> prepared the data
+        double amount = 200.0;
+        double expectedValue = 600.0;
+        double initialBalance = 800.0;
+        Account acc = AccountFactory.createAccount(initialBalance);
+
+        //Act -> Necessary actions
+        acc.withdraw(amount);
+
+        //Assert -> Expected value
+        Assertions.assertEquals(expectedValue, acc.getBalance());
+    }
+
+    @Test
+    public void withdrawShouldThrowExceptionWhenInsufficientBalance(){
+
+        //Assert -> Expected value
+        Assertions.assertThrows(IllegalArgumentException.class, ()->{
+            //Arrange -> prepared the data
+            double amount = 801.0;
+            double initialBalance = 800.0;
+            Account acc = AccountFactory.createAccount(initialBalance);
+
+            //Act -> Necessary actions
+            acc.withdraw(amount);
+        });
+    }
+
+
 }
